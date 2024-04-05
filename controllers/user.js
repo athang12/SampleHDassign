9bf9013773431977eaef31e58749098f202b45f4
athang12 <41379509+athang12@users.noreply.github.com>
2024-04-05 21:51:03 +0530

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -110,7 +110,7 @@ const register = async (req, res) => {
       };
 
       const otp = generateOTP();
-      await delay(10000);
+      await delay(5000);
       console.log(otp)
       const person = new User({
         name: username,
